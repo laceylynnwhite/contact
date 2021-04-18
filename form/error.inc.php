1746b5d3e75f8684e51d021cf533d935560f1eaa
Lacey White <laceywhite@ufl.edu>
2021-04-18 18:50:33 -0400

    Updated styling on error and thank you page

--- form/error.inc.php
+++ form/error.inc.php
@@ -1,13 +1,61 @@
-<h1>Missing fields</h1>
-<p>Sorry, you have not completed all of the required fields.</p>
-<p>Please hit <a href="#" onClick="history.go(-1)">back</a> and complete the following required fields.</p>
+<style type="text/css">
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
 
-<ul>
-<?php
-	for($i=0; $i<count($this->missing_required_fields); $i++){
-		echo "<li>" . $this->missing_required_fields[$i]['title'] . "</li>\n";
-	}
-?>
-</ul>
+  .container {
+    max-width: 1000px;
+    margin: 0 auto;
+    text-align: center;
+  }
 
-<p><strong><a href="#" onClick="history.go(-1)">Back to form</a></strong></p>
+  header {
+    text-align: center;
+    padding: 20px 0;
+  }
+
+  header nav a {
+    color: #9f6953;
+    text-decoration: none;
+  }
+
+  header nav a:hover {
+    color: rgb(255, 204, 204);
+    border-radius: 30px;
+  }
+
+  footer {
+    background: #9f9696;
+    color: rgb(255, 204, 204);
+    text-align: center;
+    padding: 30px 0;
+  }
+</style>
+
+<header>
+  <nav>
+      <a href="index.html">Contact Me</a>
+  </nav>
+</header>
+
+<div class="container"
+	<h1>Looks like you missed something!</h1>
+	<p>Sorry about that, you have not completed all of the required fields.</p>
+	<p>Please hit <a href="#" onClick="history.go(-1)">back</a> and complete the following required fields.</p>
+
+	<ul>
+	<?php
+		for($i=0; $i<count($this->missing_required_fields); $i++){
+			echo "<li>" . $this->missing_required_fields[$i]['title'] . "</li>\n";
+		}
+	?>
+	</ul>
+
+	<p><strong><a href="#" onClick="history.go(-1)">Back to form</a></strong></p>
+</div>
+
+<footer>
+  <p>Website by Lacey White</p>
+</footer>
--- form/thankyou.inc.php
+++ form/thankyou.inc.php
@@ -1,3 +1,51 @@
-<h1>Thank you for your enquiry</h1>
-<p>Thanks for your interest. we have received your message, and we will be in touch shortly.</p>
-<p>Form processor provided by <a href="http://www.inventpartners.com">Invent Partners web design York</a>.</p>
+<style type="text/css">
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
+
+  .container {
+    max-width: 1000px;
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  header {
+    text-align: center;
+    padding: 20px 0;
+  }
+
+  header nav a {
+    color: #9f6953;
+    text-decoration: none;
+  }
+
+  header nav a:hover {
+    color: rgb(255, 204, 204);
+    border-radius: 30px;
+  }
+
+  footer {
+    background: #9f9696;
+    color: rgb(255, 204, 204);
+    text-align: center;
+    padding: 30px 0;
+  }
+</style>
+
+<header>
+  <nav>
+      <a href="index.html">Contact Me</a>
+  </nav>
+</header>
+
+<div class="container">
+  <h1>Thanks for stopping by!</h1>
+  <p>I have received your message and will be in touch soon!</p>
+  <img class="contact" src="images/contact.PNG" alt="contact" title="contact" />
+</div>
+
+<footer>
+  <p>Website by Lacey White</p>
+</footer>
