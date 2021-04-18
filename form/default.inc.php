ab47ecfff8e86ce24fa5e00e80359dd0b754a68a
Lacey White <laceywhite@ufl.edu>
2021-04-18 16:56:47 -0400

    Edited default page with footer and styling

--- form/default.inc.php
+++ form/default.inc.php
@@ -1,2 +1,50 @@
-<h1>Hello</h1>
-<p>Form processor by <a href="http://www.inventpartners.com">Invent Partners Web Design York</a>.</p>
+<style type="text/css">
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
+
+  .container {
+    max-width: 1000px;
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  header {
+    text-align: center;
+    padding: 20px 0;
+  }
+
+  header nav a {
+    color: #9f6953;
+    text-decoration: none;
+  }
+
+  header nav a:hover {
+    color: rgb(255, 204, 204);
+    border-radius: 30px;
+  }
+
+  footer {
+    background: #9f9696;
+    color: rgb(255, 204, 204);
+    text-align: center;
+    padding: 30px 0;
+  }
+</style>
+
+<header>
+  <nav>
+      <a href="index.html">Contact Me</a>
+  </nav>
+</header>
+
+<div class="container">
+  <h1>Sorry, something went wrong with your submission!</h1>
+  <p>Please try again</p>
+</div>
+
+<footer>
+  <p>Website by Lacey White</p>
+</footer>
